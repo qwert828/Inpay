d08231b022fbf19856b9fc03644ba7cc79f74a3e
Aaron Phiri <aaronmtamiraphiri1@gmail.com>
2026-06-23 06:53:19 +0200

    feat: add PWA support

---
+++ public/sw.js
@@ -0,0 +1,1 @@
+const CACHE="inpay-v1";const FILES=["/","/index.html","/signup.html","/dashboard.html","/pay.html","/send.html","/pricing.html","/about.html","/docs.html"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
